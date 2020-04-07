b66c10e4c362f048f28a7508962217c9dd996832
Vivek Sharma <viveksharma03@live.com>
2020-04-07 07:50:54 +0000

    buggy implementation

---
+++ src/Game.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react"
+import Question from './Question'
+import './App.css';
+
+class Game extends Component {
+	state = {
+    	numQuestions: 0,
+      	numCorrect: 0
+    }
+	
+	updateCorrectCount() {
+    	this.setState((prevState) => ({ numCorrect: prevState.numCorrect + 1 }) )
+    }
+
+	updateQuestionCount() {
+    	this.setState((prevState) => ({ numQuestions: prevState.numQuestions + 1 }) )
+    }
+
+	render() {
+      this.updateCorrectCount = this.updateCorrectCount.bind(this)
+      this.updateQuestionCount = this.updateQuestionCount.bind(this)
+	  console.log(this.state.numQuestions)
+      return (
+          <div className="game">
+            <h2>Mental Math</h2>
+            <Question UpdateCorrectCount = {this.updateCorrectCount}
+      		  	      UpdateQuestionCount = {this.updateQuestionCount}/>
+            <p className="text">
+              Your Score: {this.state.numCorrect}/{this.state.numQuestions}
+            </p>
+          </div> 
+	  )
+    }
+}
+
+export default Game
---
+++ src/Question.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+import './App.css';
+
+
+class Question extends Component {
+  render() {
+    let value1 = Math.floor(Math.random() * 100);
+    let value2 = Math.floor(Math.random() * 100);
+    let value3 = Math.floor(Math.random() * 100);
+    let proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
+    let answer = value1 + value2 + value3 === proposedAnswer;
+    console.log(answer)
+	return (
+      <div>
+    	<div className="equation">
+            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+        </div>
+        <button onClick = {(
+			this.answer ? `
+					 this.props.UpdateCorrectCount
+					 this.props.UpdateQuestionCount
+					 `
+				   : 
+					 this.props.UpdateQuestionCount
+			)}>True</button>
+        <button onClick = {(
+			this.answer ? 
+					  this.props.UpdateQuestionCount
+					:
+					  `
+					  this.props.UpdateCorrectCount
+					  this.props.UpdateQuestionCount
+					  `)}>False</button>
+	  </div>
+    )
+  }
+}
+
+export default Question
