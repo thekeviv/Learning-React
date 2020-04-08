92f96d8da69ad54628794e65ac7ad2da5239affd
Vivek Sharma <thekeviv.vivek@gmail.com>
2020-04-08 00:07:55 -0700

    Added Contacts Project

---
+++ ContactsApp/reactnd-contacts-app/src/ListContacts.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+class ListContacts extends Component {
+  render() {
+    return (
+      <ol className="contact-list">
+        {this.props.contacts.map(contact => (
+          <li key={contact.id} className="contact-list-item">
+            <div
+              className="contact-avatar"
+              style={{ backgroundImage: `url(${contact.avatarURL})` }}
+            />
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+            </div>
+            <button
+              onClick={() => this.props.onDeleteContact(contact)}
+              className="contact-remove"
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    );
+  }
+}
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired
+};
+
+export default ListContacts;
