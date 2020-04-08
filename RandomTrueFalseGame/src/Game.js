0baa44ef4833e4f585b45c6cb7f1290fa1e54a2c
thekeviv <thekeviv.vivek@gmail.com>
2020-04-07 23:33:47 -0700

    Fixed code indentation

--- RandomTrueFalseGame/src/Game.js
+++ RandomTrueFalseGame/src/Game.js
@@ -6,29 +6,29 @@ class Game extends Component {
 	state = {
     	numQuestions: 0,
       	numCorrect: 0
-    }
+        }
 	
 	updateCorrectCount = () => {
     	this.setState((prevState) => ({ numCorrect: prevState.numCorrect + 1,
                                         numQuestions: prevState.numQuestions + 1}) )
-    }
+        }
 
 	updateQuestionCount = () => {
     	this.setState((prevState) => ({ numQuestions: prevState.numQuestions + 1 }) )
-    }
+        }
 
 	render() {
-      return (
-          <div className="game">
-            <h2>Mental Math</h2>
-            <Question UpdateCorrectCount = {this.updateCorrectCount}
-      		  	      UpdateQuestionCount = {this.updateQuestionCount}/>
-            <p className="text">
-              Your Score: {this.state.numCorrect}/{this.state.numQuestions}
-            </p>
-          </div> 
-	  )
-    }
+	      return (
+		  <div className="game">
+		    <h2>Mental Math</h2>
+		    <Question UpdateCorrectCount = {this.updateCorrectCount}
+				      UpdateQuestionCount = {this.updateQuestionCount}/>
+		    <p className="text">
+		      Your Score: {this.state.numCorrect}/{this.state.numQuestions}
+		    </p>
+		  </div> 
+	      )
+    	}
 }
 
 export default Game
