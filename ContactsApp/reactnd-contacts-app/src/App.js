4509a515ce61a95f71dd85809c28e17303df1e06
Vivek Sharma <thekeviv.vivek@gmail.com>
2020-04-13 22:31:13 -0700

    Completed implementation of CreateContact

--- ContactsApp/reactnd-contacts-app/src/App.js
+++ ContactsApp/reactnd-contacts-app/src/App.js
@@ -24,6 +24,14 @@ class App extends Component {
     }));
     ContactsAPI.remove(contact);
   };
+
+  createContact = contact => {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(currentState => ({
+        contacts: currentState.contacts.concat([contact])
+      }));
+    });
+  };
   render() {
     return (
       <div>
@@ -37,7 +45,17 @@ class App extends Component {
             />
           )}
         />
-        <Route path="/create" component={CreateContact} />
+        <Route
+          path="/create"
+          render={({ history }) => (
+            <CreateContact
+              onCreateContact={contact => {
+                this.createContact(contact);
+                history.push("/");
+              }}
+            />
+          )}
+        />
       </div>
     );
   }
